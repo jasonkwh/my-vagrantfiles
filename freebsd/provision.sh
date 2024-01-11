af0d8941f034590b4aa308c5ca401baf7c7150a2
Jason Huang <jasonkwh@gmail.com>
2024-01-11 16:34:49 +1100

    fix provision.sh

--- freebsd/provision.sh
+++ freebsd/provision.sh
@@ -6,10 +6,10 @@ pkg install -y neofetch curl tmux git go rust zsh neovim
 
 # change default shell to Zsh
 chsh -s /usr/local/bin/zsh vagrant
-export HOME=/home/vagrant
 
 # # install oh-my-zsh
-if [ ! -d "/root/.oh-my-zsh" ]; then
+if [ ! -d "/home/vagrant/.oh-my-zsh" ]; then
+    export HOME=/home/vagrant # for oh-my-zsh installation
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
@@ -18,10 +18,14 @@ if [ ! -d "/home/vagrant/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; t
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git /home/vagrant/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 fi
 
-if [ ! -d "/root/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+if [ ! -d "/home/vagrant/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions /home/vagrant/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 fi
 
-if [ ! -d "/root/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
+if [ ! -d "/home/vagrant/.oh-my-zsh/custom/themes/powerlevel10k" ]; then
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /home/vagrant/.oh-my-zsh/custom/themes/powerlevel10k
+fi
+
+if [ ! -d "/home/vagrant/my-vagrantfiles" ]; then
+    git clone https://github.com/jasonkwh/my-vagrantfiles.git /home/vagrant/my-vagrantfiles
 fi
